7b2121c693a342b504e66a70833a85c583810174
SQALviv <dayok.org@gmail.com>
2013-07-18 15:03:22 +0300

    xcxc
    xcxc

---
+++ sqa/main/java/com/dayok/concepts/Repository.java
@@ -0,0 +1,14 @@
+package com.dayok.concepts;
+
+import com.dayok.pages.MainPage;
+import com.dayok.verifications.FixedMenuAndLogoVerification;
+import com.dayok.webdriver.PropertiesData;
+
+public interface Repository {
+
+	PropertiesData getPropertiesData();
+	
+	MainPage getMainPage();
+	
+	FixedMenuAndLogoVerification getFixedMenuVerification();
+}
