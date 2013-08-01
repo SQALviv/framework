36183641c427e72d5c3e46d8409a88daa66e47aa
SQALviv <denis.zayats@gmail.com>
2013-08-01 11:14:11 +0300

    Add explicit wait example

--- sqa/src/main/java/com/sqa/pages/BrandPage.java
+++ sqa/src/main/java/com/sqa/pages/BrandPage.java
@@ -2,6 +2,8 @@ package com.sqa.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.sqa.webdriver.FirefoxWebDriver;
 
@@ -11,5 +13,12 @@ public class BrandPage  extends FirefoxWebDriver{
 	public WebElement getBrandName(){
 		return driver.findElement(By.xpath("//*[@id='main']/h2"));
 	}
+	
+	
+	public static WebElement explicitWait()  
+	 {  
+	      WebDriverWait wait = new WebDriverWait(driver, 30);  
+	      return wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("")));  
+	 }   
 
 }
